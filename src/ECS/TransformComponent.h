fc3ad8cfee0421f9230cf1947dc09d6253a1a232
tranthanhvan235 <98211651+tranthanhvan235@users.noreply.github.com>
2024-03-14 15:47:09 +0700

    add missing file after pre commit

--- src/ECS/TransformComponent.h
+++ src/ECS/TransformComponent.h
@@ -9,12 +9,21 @@ public:
     Vector2D position;
     Vector2D velocity;
     
+    int height = 32;
+    int width = 32;
+    int scale = 1;
+
     int speed = 3;
 
     TransformComponent()
     {
-        position.x = 0.0f;
-        position.y = 0.0f;
+        position.Zero();
+    }
+
+    TransformComponent(int sc)
+    {
+        position.Zero();
+        scale = sc;
     }
 
     TransformComponent(float x, float y)
@@ -23,10 +32,18 @@ public:
         position.y = y;
     }
 
+    TransformComponent(float x, float y, int h, int w, int sc)
+    {
+        position.x = x;
+        position.y = y;
+        height = h;
+        width = w;
+        scale = sc;
+    }
+
     void init() override
     {
-        velocity.x = 0;
-        velocity.y = 0;
+        velocity.Zero();
     }
     void update() override
     {
--- src/Game.cpp
+++ src/Game.cpp
@@ -3,14 +3,22 @@
 #include "Map.h"
 #include "ECS/Components.h"
 #include "Vector2D.h"
+#include "Collision.h"
 
 Map *map;
 Manager manager;
 
 SDL_Renderer *Game::renderer = nullptr;
 SDL_Event Game::event;
-    
-auto& player(manager.addEntity());
+
+std::vector<ColliderComponent *> Game::colliders;
+
+auto &player(manager.addEntity());
+auto &wall(manager.addEntity());
+
+auto &tile0(manager.addEntity());
+auto &tile1(manager.addEntity());
+auto &tile2(manager.addEntity());
 
 Game::Game()
 {
@@ -22,6 +30,7 @@ Game::~Game()
 void Game::init(const char *title, int xpos, int ypos, int width, int height, bool fullscreen)
 {
     int flags = 0;
+
     if (fullscreen)
     {
         flags = SDL_WINDOW_FULLSCREEN;
@@ -41,17 +50,28 @@ void Game::init(const char *title, int xpos, int ypos, int width, int height, bo
         isRunning = true;
     }
     map = new Map();
-    
-    //ecs implementation
 
-    player.addComponent<TransformComponent>(); 
+    // ecs implementation
+
+    tile0.addComponent<TileComponent>(200, 200, 32, 32, 0);
+    tile1.addComponent<TileComponent>(250, 250, 32, 32, 1);
+    tile1.addComponent<ColliderComponent>("dirt");
+    tile2.addComponent<TileComponent>(150, 150, 32, 32, 2);
+    tile2.addComponent<ColliderComponent>("grass");
+
+    player.addComponent<TransformComponent>(2);
     player.addComponent<SpriteComponent>("assets/snake2d.png");
     player.addComponent<KeyboardController>();
+    player.addComponent<ColliderComponent>("player");
+
+    wall.addComponent<TransformComponent>(300.0f, 300.0f, 300, 20, 1);
+    wall.addComponent<SpriteComponent>("assets/dirt.png");
+    wall.addComponent<ColliderComponent>("wall");
 }
 
 void Game::handleEvents()
 {
-        SDL_PollEvent(&event);
+    SDL_PollEvent(&event);
 
     switch (event.type)
     {
@@ -68,12 +88,18 @@ void Game::update()
 {
     manager.refresh();
     manager.update();
+    // Vector2D playerPos = player.getComponent<TransformComponent>().position;
+
+    for (auto cc : colliders)
+    {
+        Collision::AABB(player.getComponent<ColliderComponent>(), *cc);
+    }
 }
 
 void Game::render()
 {
     SDL_RenderClear(renderer);
-    map->DrawMap();
+    // map->DrawMap();
 
     manager.draw();
     SDL_RenderPresent(renderer);
