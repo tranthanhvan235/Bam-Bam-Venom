1e8ecf06879cf5cb3f2e4addb42c1ed96948e56c
tranthanhvan235 <98211651+tranthanhvan235@users.noreply.github.com>
2024-03-29 22:16:39 +0700

    unfolder some font

---
+++ src/ultils.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <string>
+#include <stdlib.h>
+#include <fstream>
+#include <ostream>
+
+#include "Texture.h"
+#include "const.h"
+#include "Res.h"
+
+
+int random(const int &min, const int &max);
+
+void showLive(SDL_Renderer *renderer);
+
+void showScore(SDL_Renderer *renderer);
+
+void readHighestScore();
+void updateHighestScore();
+void showHighestScore(SDL_Renderer *renderer);
+
+void levelUp();
