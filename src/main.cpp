44d34a73f5280a2a90dd667af4eaabb52fa452e0
tranthanhvan235 <98211651+tranthanhvan235@users.noreply.github.com>
2024-04-03 19:00:58 +0700

    main.cpp after trans init, load, quit to a function in game class
    + remove renderer arg in func

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
-#include "Game.h"
 #include "Res.h"
+#include "Game.h"
 
-SDL_Window *window;
-SDL_Renderer *renderer;
 Game game;
 
 int score;
@@ -14,12 +12,12 @@ int main(int argc, char *args[])
 {
     srand(time(NULL));
     
-    if(init(window, renderer))
-        load(renderer);
+    if(game.init())
+        game.load();
     
     game.setGameState(MENU);
-    game.manageState(renderer);
+    game.manageState();
 
-    quit(window, renderer);
+    game.quit();
     return 0;
 }
