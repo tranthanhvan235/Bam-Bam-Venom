d097e886ff23fa171c0dc9dc2437b904863a31d5
tranthanhvan235 <98211651+tranthanhvan235@users.noreply.github.com>
2024-04-15 11:50:50 +0700

    update fruit
    thêm collision cho fruit và fruit chỉ chạy một loại quả thôi

--- src/Fruit.cpp
+++ src/Fruit.cpp
@@ -3,123 +3,99 @@
 // Constructor and destructor
 Fruit::Fruit()
 {
-    update();
+    int i = rand() % 9 + 1;
+    i = LEMON;
+    switch (i)
+    {
+    case GRAPE:
+        loadImage(fruit, "assets/fruit/grape.png");
+        break;
+
+    case PINEAPPLE:
+        loadImage(fruit, "assets/fruit/pineapple.png");
+        break;
+
+    case CHERRY:
+        loadImage(fruit, "assets/fruit/cherry.png");
+        break;
+
+    case LEMON:
+        loadImage(fruit, "assets/fruit/lemon.png");
+        break;
+
+    case WATERMELON:
+        loadImage(fruit, "assets/fruit/watermelon.png");
+        break;
+
+    case KIWI:
+        loadImage(fruit, "assets/fruit/kiwi.png");
+        break;
+
+    case STRAWBERRY:
+        loadImage(fruit, "assets/fruit/strawberry.png");
+        break;
+
+    case PEAR:
+        loadImage(fruit, "assets/fruit/pear.png");
+        break;
+    case ORANGE:
+        loadImage(fruit, "assets/fruit/orange.png");
+        break;
+
+    default:
+        break;
+    }
+    posY = 500;
+    fruitCol.setCollision(posX, posY, fruit.getWidth() - 50, fruit.getHeight() - 50);
+    frame = 0;
+    timer.start();
+    posX = -fruit.getWidth();
+    velX = 1.5;
 }
 
 Fruit::~Fruit()
 {
 }
-void Fruit::update()
+void Fruit::moveFruit()
 {
-    for (int i = 0; i < NUM_FRUIT; i++)
+    posX += velX;
+
+    if (posX + fruit.getWidth() > SCREEN_WIDTH)
     {
-        switch (i)
-        {
-        case GRAPE:
-            loadImage(fruit[i], "assets/fruit/grape.png");
-            break;
-
-        case PINEAPPLE:
-            loadImage(fruit[i], "assets/fruit/pineapple.png");
-            break;
-
-        case CHERRY:
-            loadImage(fruit[i], "assets/fruit/cherry.png");
-            break;
-
-        case LEMON:
-            loadImage(fruit[i], "assets/fruit/lemon.png");
-		    break;
-
-        case WATERMELON:
-            loadImage(fruit[i], "assets/fruit/watermelon.png");
-            break;
-
-        case KIWI:
-            loadImage(fruit[i], "assets/fruit/kiwi.png");
-            break;
-
-        case STRAWBERRY:
-            loadImage(fruit[i], "assets/fruit/strawberry.png");
-            break;
-
-        case PEAR:
-            loadImage(fruit[i], "assets/fruit/pear.png");
-            break;
-        case ORANGE:
-            loadImage(fruit[i], "assets/fruit/orange.png");
-            break;
-
-        default:
-            break;
-        }
-    }
-    for (int i = 0; i < NUM_FRUIT; i++)
-    {
-        frame[i] = 0;
-        timer[i].start();
-        posX[i] = -fruit[i].getWidth() - 50;
-        posY[i] = rand()%(SCREEN_HEIGHT - fruit[i].getHeight() - 90 + 1) + 90;
-        velX[i] = rand()%maxVel + 1.5;
-        velY[i] = rand()%maxVel + 1.5;
-    }
-}
-void Fruit::moveFruit(int i)
-{
-    posX[i] += velX[i];
-
-    if(posX[i] + fruit[i].getWidth() > SCREEN_WIDTH)
-    {
-        posX[i] = -fruit[i].getWidth();
-        posY[i] = rand()% (SCREEN_HEIGHT - fruit[i].getHeight() - 20);
-        velX[i] = rand()%maxVel + 1.5;
+        posX = -fruit.getWidth();
+        velX = 1.5; //rand() % maxVel + 1.5;
     }
 
-    posY[i] += velY[i] * 0.35;
-
-    if(posY[i] <= 86)
-    {
-        velY[i] = -velY[i];
-        posY[i] += velY[i];
-    }
-
-    if(posY[i] + fruit[i].getHeight() > SCREEN_HEIGHT - 10)
-    {
-        velY[i] = -velY[i];
-        posY[i] += velY[i];
-    }
 }
 // Render
-void Fruit::render(int i)
+void Fruit::render()
 {
-    if(frame[i] == 3) frame[i] = 0;
-    int index = 3 * i + frame[i];
-    fruit[i].render(posX[i], 500, 166, 166, NULL);
-    if(timer[i].get_ticks() % 15 == 0) frame[i]++;
+    fruitCol.setCollision(posX + 40, posY + 40, fruit.getWidth() - 80, fruit.getHeight() - 80);
+    fruit.render(posX, posY, fruit.getHeight(), fruit.getHeight(), NULL);
 }
 
-void Fruit::generate(int i)
+void Fruit::generate()
 {
-    moveFruit(i);
-    render(i);
+    moveFruit();
+    render();
 }
 
-double Fruit::getPosX(int i)
+double Fruit::getPosX()
 {
-    return posX[i];
+    return posX;
 }
 
-double Fruit::getPosY(int i)
+double Fruit::getPosY()
 {
-    return posY[i];
+    return posY;
 }
 
-double Fruit::getWidth(int i)
+double Fruit::getWidth()
 {
-    return fruit[i].getWidth();
+    return fruit.getWidth();
 }
 
-double Fruit::getHeight(int i)
+double Fruit::getHeight()
 {
-    return fruit[i].getHeight();
+    return fruit.getHeight();
 }
--- src/Fruit.h
+++ src/Fruit.h
@@ -3,6 +3,7 @@
 #include "const.h"
 #include "Timer.h"
 #include "Res.h"
+#include "Collision.h"
 
 class Fruit
 {
@@ -11,25 +12,24 @@ public:
     Fruit();
     ~Fruit();
 
-    void update();
-
     // Render
-    void render(int i);
+    void render();
 
-    void moveFruit(int i);
+    void moveFruit();
 
-    void generate(int i);
+    void generate();
 
-    double getPosX(int i);
-    double getPosY(int i);
-    double getWidth(int i);
-    double getHeight(int i);
+    double getPosX();
+    double getPosY();
+    double getWidth();
+    double getHeight();
 
-    double posX[NUM_FRUIT], posY[NUM_FRUIT];
-    double velX[NUM_FRUIT], velY[NUM_FRUIT];
+    double posX, posY;
+    double velX, velY;
+    Collision fruitCol;
 
 private:
-    double frame[NUM_FRUIT];
-    Timer timer[NUM_FRUIT];
-    Texture fruit[NUM_FRUIT];
+    double frame;
+    Timer timer;
+    Texture fruit;
 };
